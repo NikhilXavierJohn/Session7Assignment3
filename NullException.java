b0dd88725c90c5843c5a4f0d666c1d59365a7cbe
NikhilXavierJohn <nkh.john@gmail.com>
2016-08-27 15:56:40 +0530

    Add files via upload

---
+++ NullException.java
@@ -0,0 +1,21 @@
+public class NullException{
+
+	public static void main(String[] args) {
+		
+		String a = null;
+		
+		try{
+			
+			if(a.equals("abc"))
+				System.out.println(a);
+			
+		}
+		catch(Exception e){
+			
+			System.out.println("Exception Handled :"+e);
+			
+		}
+
+	}
+
+}
